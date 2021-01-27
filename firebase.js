fa14bf484ee9e3e2262ccc622883ed9c64e8c506
Aminata Kabba <aminata_kabba@hotmail.com>
2021-01-27 19:27:28 +0000

    firs commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCMiIt4lMbFvmDUv5-tUjg7u-anWaEHcKE",
+    authDomain: "signal-clone-f0b80.firebaseapp.com",
+    projectId: "signal-clone-f0b80",
+    storageBucket: "signal-clone-f0b80.appspot.com",
+    messagingSenderId: "404886937606",
+    appId: "1:404886937606:web:4723577a695d6f31456387",
+    measurementId: "G-RKL94QG78P"
+  };
